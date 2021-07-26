cbc4c6a88420248b0870727c870dbf2040a24f1f
minh-p <minhisverynormal@gmail.com>
2021-07-26 08:53:16 -0700

    Button stuff

--- scripts/sectionScrolling.js
+++ scripts/sectionScrolling.js
@@ -1,8 +1,23 @@
+console.log("Hello World");
+
 // Section Scrolling
 
-function scroll() {
+const homeSection = document.getElementById("homeSection");
+const aboutSection = document.getElementById("aboutSection");
+
+function scroll(section) {
     // Scrolling functionality?
     // Math to get the appropriate scrolling position?
+    window.scrollTo(0, section.scrollTop);
 }
 
 // Event listening when navigation links are pressed?
+const navigationLinks = document.getElementsByClassName("navigationLinks");
+
+// Set up navigationLinks click event.
+for (let i = 0; i < navigationLinks.length; i++) {
+    navigationLinks[i].onclick = function() {
+        console.log("Link clicked");
+        scroll(aboutSection);
+    }
+}
