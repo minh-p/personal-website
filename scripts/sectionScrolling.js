e3c9701aafa4bb55547a4732c83a84863ae3e606
minh-p <minhisverynormal@gmail.com>
2021-07-25 19:37:33 -0700

    Created sectionScrolling.js

---
+++ scripts/sectionScrolling.js
@@ -0,0 +1,8 @@
+// Section Scrolling
+
+function scroll() {
+    // Scrolling functionality?
+    // Math to get the appropriate scrolling position?
+}
+
+// Event listening when navigation links are pressed?
