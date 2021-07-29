ec62dafc157cb97102d12d3aabe8f8b01b125a2e
minh-p <minhisverynormal@gmail.com>
2021-07-29 12:47:38 -0700

    figured out how to get sectionId from navigationLinkElement's href

--- scripts/sectionScrolling.js
+++ scripts/sectionScrolling.js
@@ -1,23 +1,35 @@
-console.log("Hello World");
-
 // Section Scrolling
 
 const homeSection = document.getElementById("homeSection");
 const aboutSection = document.getElementById("aboutSection");
 
+function getSectionFromNavigationLink(navigationLinkElement) {
+    /*
+     * There is a hashtag in the href/url.
+     * To get the sectionId, we remove everything before it, in the href string.
+    */
+    const hashtagPosition = navigationLinkElement.href.search('#');
+    const sectionId = navigationLinkElement.href.slice(hashtagPosition + 1).concat("Section");
+    return document.getElementById(sectionId);
+}
+
 function scroll(section) {
     // Scrolling functionality?
     // Math to get the appropriate scrolling position?
-    window.scrollTo(0, section.scrollTop);
+
+    if (!section) console.log("no section"); return;
 }
 
 // Event listening when navigation links are pressed?
-const navigationLinks = document.getElementsByClassName("navigationLinks");
+const navigationLinks = document.getElementsByClassName("navigationLink");
 
 // Set up navigationLinks click event.
 for (let i = 0; i < navigationLinks.length; i++) {
     navigationLinks[i].onclick = function() {
-        console.log("Link clicked");
-        scroll(aboutSection);
+        scroll(getSectionFromNavigationLink(navigationLinks[i]));
     }
 }
+
+/* get current scroll top position like this:
+ * document.documentElement.scrollTop
+*/
