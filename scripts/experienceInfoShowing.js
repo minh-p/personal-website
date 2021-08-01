e4340e3ce0e3e736011ce33d20ee7b78a8ae9dcb
minh-p <minhisverynormal@gmail.com>
2021-08-01 15:12:57 -0700

    Finished making experience Info system

--- scripts/experienceInfoShowing.js
+++ scripts/experienceInfoShowing.js
@@ -6,11 +6,16 @@
  * experiencesButtons' ids will be the keys to the experienceInfo's to display
  */
 
+import experienceInfos from './experienceInfos.js';
+
 const experiencesButtons = document.getElementsByClassName("experiencesButton");
+const experienceInfoParagraph = document.getElementById("experiencesInfo");
 
 for (let i = 0; i < experiencesButtons.length; i++) {
     const button  = experiencesButtons[i];
     button.onclick = function() {
-        console.log("experience button clicked");
+        const experiencesInfo = experienceInfos[button.id];
+        if (!experiencesInfo) return;
+        experienceInfoParagraph.innerHTML = experienceInfos[button.id];
     }
 }
---
+++ scripts/experienceInfos.js
@@ -0,0 +1,6 @@
+const experienceInfos = {
+    robloxStudio: "Roblox Studio is the game engine of Roblox. Used by developers to create games on the platform.<br>2 Years of Experience: (2019-2022)",
+    python: "Python is currently a very popular language used in many applications. I know most of Python syntax, how to work with modules...<br>I am planning to use it with Web Development and a little bit of Machine Learning."
+};
+
+export default experienceInfos;
